e5c20a22115018e86dfd5e9b42a473a22be8814b
Kookri <69087621+Kookri@users.noreply.github.com>
2023-05-21 12:57:26 -0400

    messing with app.js to display a basic form
    now shows a very simple signin form that sends info to the browser console. Next step is configuring it chatting with the database

---
+++ habit-hero/src/SignupForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const SignupForm = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Here you would send the form data to your server
+    console.log(username, email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignupForm;
