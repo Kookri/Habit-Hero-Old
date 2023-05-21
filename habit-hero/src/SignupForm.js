49bbc946af506b098eeee40cbd6a24db9066e035
Kookri <69087621+Kookri@users.noreply.github.com>
2023-05-21 13:29:09 -0400

    User sign up
    Users can now sign up and it adds their login info to the database. It appears to hash the password. Still need to test whether you can log in with these credentials.

--- habit-hero/src/SignupForm.js
+++ habit-hero/src/SignupForm.js
@@ -8,36 +8,51 @@ const SignupForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Here you would send the form data to your server
-    console.log(username, email, password);
+    // Make a POST request to your backend API
+    try {
+      const response = await fetch("http://localhost:5000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      if (response.ok) {
+        // Successful user creation, handle the response as needed
+        const data = await response.json();
+        console.log(data); // For testing purposes, you can log the response
+      } else {
+        // Handle user creation error
+        const errorData = await response.json();
+        console.log(errorData); // Log the error response
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
+      {/* Input fields for username, email, and password */}
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Sign Up</button>
     </form>
   );
