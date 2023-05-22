cb0c7e831912c7070f0f2529d6dcabf6d3f4ee69
Kookri <69087621+Kookri@users.noreply.github.com>
2023-05-22 16:32:54 -0400

    contexts - sign in form
    Attempting to update the sign in form to show the username, using contexts. Still haven't figured it out. I've hamfistedly displayed a "UserProfile" which isn't yet correctly configured, which is intended to show the username when signed in.
    
    Now I need to somehow pass username to userprofile out of mongodb.

--- habit-hero/src/App.js
+++ habit-hero/src/App.js
@@ -2,20 +2,21 @@ import React, { useContext } from 'react';
 import { UserContext } from './UserContext'; // ensure that './UserContext' correctly points to the UserContext.js file
 import SignupForm from './SignupForm';
 import SigninForm from './SigninForm';
+import UserProfile from './UserProfile';
 
 const App = () => {
   const { user } = useContext(UserContext);
 
   return (
     <div className="App">
-      {/* If the user is signed in, display a welcome message with their username. */}
-      {user ? <h1>Hello, {user.username}!</h1> : null}
-      
       <h2>Sign up</h2>
       <SignupForm />
 
       <h2>Or Sign In</h2>
       <SigninForm />
+
+      <h2>User Profile</h2>
+      <UserProfile />
     </div>
   );
 };
--- models/User.js
+++ models/User.js
@@ -1,43 +1,57 @@
+// This model contains the database schema for users, and handles hashing passwords and storing the data in the mongodb. 
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({  
-    username: {  
-      type: String,  
-      required: true,  
-      unique: true,  
-    },  
-    email: {  
-      type: String,  
-      required: true,  
-      unique: true,  
-    },  
-    password: {  
-      type: String,  
-      required: true,  
-    },  
-    refreshToken: {  
-      type: String,  
-    }  
-  });
-
-UserSchema.pre('save', function(next) {
-    const user = this;
-
-    if(!user.isModified('password')) {
-        return next();
-    }
-
-    bcrypt.hash(user.password, 10, (err, hash) => {
-        if(err) {
-            return next(err);
-        }
-
-        user.password = hash;
-        next();
-    });
+// Define the UserSchema using mongoose.Schema
+const UserSchema = new mongoose.Schema({
+  // Define the username field
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // Define the email field
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // Define the password field
+  password: {
+    type: String,
+    required: true,
+  },
+  // Define the refreshToken field
+  refreshToken: {
+    type: String,
+  }
 });
 
+// Define a pre-save middleware that automatically hashes the password before saving the user
+UserSchema.pre('save', function(next) {
+  const user = this;
+
+  // If the password is not modified, proceed to the next middleware
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // Hash the password using bcrypt with a salt of 10 rounds
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    // Replace the user's password with the hashed password
+    user.password = hash;
+    next();
+  });
+});
+
+// Create the User model using mongoose.model
 const User = mongoose.model('User', UserSchema);
 
+// Export the User model 
+// By exporting the User model, it can be imported and used in other parts of the application, such as the routes or controllers, to perform CRUD (Create, Read, Update, Delete) operations on user data.
 module.exports = User;
