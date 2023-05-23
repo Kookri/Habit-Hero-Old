0a7e2dc4e35ed8661f05149fc80db65a9fc67911
Kookri <69087621+Kookri@users.noreply.github.com>
2023-05-23 12:29:43 -0400

    Auth.js refactoring
    Made more readable.

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const User = require('../models/UserModel');
 const bcrypt = require('bcrypt');
 const config = require('../config');
+const User = require('../models/UserModel');
 
 const router = express.Router();
 
+// User sign-up route
 router.post('/signup', async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
+    // Create a new user instance
     const user = new User({ username, email, password });
     await user.save();
 
+    // Generate access token and refresh token
     const accessToken = jwt.sign({ userId: user._id }, config.jwtSecret, { expiresIn: '15m' });
     const refreshToken = jwt.sign({ userId: user._id }, config.jwtSecret, { expiresIn: '7d' });
 
@@ -26,6 +29,7 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+// User login route
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
@@ -34,16 +38,21 @@ router.post('/login', async (req, res) => {
   }
 
   try {
+    // Find the user with the provided email
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(422).send({ error: 'Invalid email' });
     }
 
+    // Compare the provided password with the hashed password stored in the user model
     const passwordMatch = await bcrypt.compare(password, user.password);
+
     if (!passwordMatch) {
       return res.status(422).send({ error: 'Invalid password' });
     }
 
+    // Generate access token and refresh token
     const accessToken = jwt.sign({ userId: user._id }, config.jwtSecret, { expiresIn: '15m' });
     const refreshToken = jwt.sign({ userId: user._id }, config.jwtSecret, { expiresIn: '7d' });
 
@@ -57,6 +66,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// Token refresh route
 router.post('/token', async (req, res) => {
   const { refreshToken } = req.body;
 
@@ -86,8 +96,8 @@ router.post('/token', async (req, res) => {
     // Send the new tokens
     res.send({ accessToken, refreshToken: newRefreshToken });
   } catch (err) {
-    return res.status(422).send({ error: 'Invalid refresh token' });
+    return res.status(422).send({ error: 'Invalid refreshtoken' });
   }
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
